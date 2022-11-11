b98823f69082559a940a49d19931b3c48df34112
Nguyễn Quang Sáng <89448644+sangsura@users.noreply.github.com>
2022-11-11 22:20:13 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
 ten=st.text_input("Nhập tên vào đây:")
 
 if ten:
